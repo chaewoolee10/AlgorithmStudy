c67b5dac7371e208337473f76e9f209167ceba0e
chaewoolee10 <chaewoolee10@gmail.com>
2024-12-31 11:56:57 +0900

    .

---
+++ 0x02/0x02_Problem1_10871.c
@@ -0,0 +1,23 @@
+//10871
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+    int x; scanf("%d", &x);
+    int arr[10000] = {};
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);    
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] < x)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    
+    
+}
